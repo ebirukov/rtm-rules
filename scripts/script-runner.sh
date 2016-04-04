b8edaad25e1c74e99216e6a1a010cf3f2ca708e8
Ubuntu <ubuntu@ip-172-31-21-95.ec2.internal>
2016-04-04 16:34:22 +0000

    test

--- scripts/script-runner.sh
+++ scripts/script-runner.sh
@@ -5,11 +5,18 @@ cd /usr/local/spark/bin/
 if aws s3 cp $1 $file ; then
 chmod +x $file
 
+JARS="/usr/local/spark/lib/spark-streaming-kinesis-asl.jar"
+for bindir in /usr/local/bin/rtm-rules/lib/*; do
+        JARS=$JARS,${bindir}
+done
+echo $JARS
+
+
 docker-run-spark-env.sh $SPARK_HOME/bin/spark-submit \
 --master local[*] \
 --driver-memory $DRIVER_MEMORY \
 --driver-class-path $SPARK_DRIVER_CLASSPATH \
---jars $SPARK_JARS \
+--jars $JARS \
 $file $ARGS 2>&1
 
 else
